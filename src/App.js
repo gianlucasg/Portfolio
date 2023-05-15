f3bae877a1cb6b133b87435749bdbd9b13a03a2d
Gianlucas Galati <gianlucasgalati11@gmail.com>
2023-05-14 22:22:30 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import Home from './pages/home'
 import Hireme from './pages/hireme'
 import Projects from './pages/projects'
 import AboutMe from './components/layout/aboutme/aboutme'
-import { BrowserRouter ,Route,Routes} from "react-router-dom";
+// import { BrowserRouter ,Route,Routes} from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
           <Container>
           <NavBar/>
           <div class='container-flex'>
             <Box class='card-left'>
               <Routes>
-                <Route index path="portfolio/" element={<Home/>} />
-                <Route index path="portfolio/projects" element={<Projects/>} />
-                <Route index path="portfolio/hireme" element={<Hireme/>} />
+                <Route index path="/" element={<Home/>} />
+                <Route index path="/projects" element={<Projects/>} />
+                <Route index path="/hireme" element={<Hireme/>} />
               </Routes>
             </Box>
             <Box class='card-right'>
@@ -28,7 +29,7 @@ function App() {
           </div>
         </Container>
           <ParticlesBackground/>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
